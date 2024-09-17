e4589419d1a3e81bfe3591f1d4b20bb6f5d742f4
Abubakar Imoro <abubakarimoro27@gmail.com>
2024-09-17 15:48:52 +0000

    success message

--- scripts/register.js
+++ scripts/register.js
@@ -1,7 +1,25 @@
 // get form element by id
-const registerForm = document.getElementById('register');
+const registerForm = document.querySelector("#register");
 // handle form submit event
-registerForm.onsubmit = function (event) {
-event.preventDefault()
-    console.log(event);
-}
+registerForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+// collect form data
+const formData = new FormData(registerForm);
+console.log(formData.get('username'), formData.get('email'), formData.get('password'))
+
+// send data to backend
+// display success message to user
+const messageH1 = document.querySelector('#message');
+messageH1.textContent = 'Account registered successfully!'
+});
+
+
+// // declare variable age
+// const age = 71;
+// // define square age function
+// function squareage(age){ 
+//     return age**2;
+// }
+
+// const squareAge = squareage(34);
+// console.log(squareAge)
